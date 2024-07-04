e3bd01f7e7a3ecca27c9c83e1e8b6f66383045c6
Felipe Coury <felipe.coury@gmail.com>
2024-07-04 20:51:06 -0300

    feat: simplified command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,26 @@
-use clap::{Parser, Subcommand};
+use clap::Parser;
 use husk::{execute_script, repl};
 
 #[derive(Parser, Debug)]
 struct Cli {
-    #[clap(subcommand)]
-    command: Command,
-}
-
-#[derive(Subcommand, Debug)]
-enum Command {
-    /// Executes a husk script
-    Run { file: std::path::PathBuf },
-
-    /// Runs the husk REPL
-    Repl,
+    /// Run a Husk script
+    file: Option<std::path::PathBuf>,
 }
 
 fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
 
-    match cli {
-        Cli {
-            command: Command::Run { file },
-        } => {
-            let code = std::fs::read_to_string(file)?;
-            match execute_script(&code) {
-                Ok(_) => {}
-                Err(e) => {
-                    eprintln!("{}", e.pretty_print(code));
-                    std::process::exit(1);
-                }
+    if let Some(file) = cli.file {
+        let code = std::fs::read_to_string(file)?;
+        match execute_script(&code) {
+            Ok(_) => {}
+            Err(e) => {
+                eprintln!("{}", e.pretty_print(code));
+                std::process::exit(1);
             }
         }
-        Cli {
-            command: Command::Repl,
-        } => {
-            let _ = repl();
-        }
+    } else {
+        let _ = repl();
     }
 
     Ok(())
