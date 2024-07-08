d24ec2c886d72759c7083d37150c5f800a40c5f8
Felipe Coury <felipe.coury@gmail.com>
2024-07-07 21:31:12 -0300

    feat: transpile husk code to javascript

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ mod parser;
 mod repl;
 mod semantic;
 mod span;
+mod transpiler;
 
 pub use error::{Error, Result};
 pub use interpreter::{Interpreter, Value};
@@ -12,6 +13,7 @@ pub use lexer::Lexer;
 pub use parser::Parser;
 pub use repl::repl;
 pub use semantic::SemanticAnalyzer;
+use transpiler::JsTranspiler;
 
 pub fn execute_script(code: impl Into<String>) -> Result<Value> {
     let code = code.into();
@@ -26,4 +28,19 @@ pub fn execute_script(code: impl Into<String>) -> Result<Value> {
 
     let mut interpreter = Interpreter::new();
     interpreter.interpret(&ast)
+}
+
+pub fn transpile_to_js(code: impl Into<String>) -> Result<String> {
+    let code = code.into();
+    let mut lexer = Lexer::new(code.to_string());
+    let tokens = lexer.lex_all();
+
+    let mut parser = Parser::new(tokens);
+    let ast = parser.parse()?;
+
+    let mut analyzer = SemanticAnalyzer::new();
+    analyzer.analyze(&ast)?;
+
+    let js_generator = JsTranspiler::new();
+    Ok(js_generator.generate(&ast))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,78 @@
-use clap::Parser;
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
 use husk::{execute_script, repl};
 
 #[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
 struct Cli {
-    /// Run a Husk script
+    /// Optional subcommand
+    #[clap(subcommand)]
+    cmd: Option<Command>,
+
+    /// Run a hash script
     file: Option<std::path::PathBuf>,
+}
+
+#[derive(Parser, Debug)]
+struct Run {
+    /// Run a Husk script
+    file: PathBuf,
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    /// Start the Husk REPL
+    Repl,
+
+    /// Run a Husk script
+    Run(Run),
+
+    /// Transpile a Husk script to JavaScript
+    Compile(Compile),
+}
+
+#[derive(Parser, Debug)]
+struct Compile {
+    /// The Husk script to transpile
+    file: std::path::PathBuf,
 }
 
 fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
 
     if let Some(file) = cli.file {
-        let code = std::fs::read_to_string(file)?;
-        match execute_script(&code) {
-            Ok(_) => {}
-            Err(e) => {
-                eprintln!("{}", e.pretty_print(code));
-                std::process::exit(1);
-            }
+        return run_command(file);
+    }
+
+    Ok(match cli.cmd {
+        Some(Command::Run(run)) => run_command(run.file)?,
+        Some(Command::Compile(compile)) => compile_command(compile)?,
+        Some(Command::Repl) | None => repl()?,
+    })
+}
+
+fn run_command(file: PathBuf) -> anyhow::Result<()> {
+    let code = std::fs::read_to_string(file)?;
+    match execute_script(&code) {
+        Ok(_) => {}
+        Err(e) => {
+            eprintln!("{}", e.pretty_print(code));
+            std::process::exit(1);
         }
-    } else {
-        let _ = repl();
+    }
+
+    Ok(())
+}
+
+fn compile_command(cli: Compile) -> anyhow::Result<()> {
+    let code = std::fs::read_to_string(cli.file)?;
+    match husk::transpile_to_js(&code) {
+        Ok(js) => println!("{}", js),
+        Err(e) => {
+            eprintln!("{}", e.pretty_print(code));
+            std::process::exit(1);
+        }
     }
 
     Ok(())
