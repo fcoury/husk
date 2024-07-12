b32460a69dff108474be289e6d52f2f6faf8d482
Felipe Coury <felipe.coury@gmail.com>
2024-07-12 00:16:51 -0300

    feat: transpiler integrated test

--- tests/scripts/test.sh
+++ tests/scripts/test.sh
@@ -5,9 +5,19 @@ SCRIPT_DIR="tests/scripts"
 # Temporary files to capture stdout and stderr
 TEMP_STDOUT="temp-stdout"
 TEMP_STDERR="temp-stderr"
-
 # Variable to track if any test fails
 ANY_TEST_FAILED=0
+# Flag for transpile mode
+TRANSPILE_MODE=0
+
+# Check for transpile option
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -t|--transpile) TRANSPILE_MODE=1 ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
 
 # Loop through all .hk files in the specified directory
 for file in "$SCRIPT_DIR"/*.hk; 
@@ -17,39 +27,57 @@ do
     echo "No .hk files found in $SCRIPT_DIR."
     exit 1
   fi
-
+  
   # Extract the base name of the file (without extension)
   base_name=$(basename "$file")
 
   # Run the script and capture the output
-  ./target/debug/husk "$file" > "$TEMP_STDOUT" 2> "$TEMP_STDERR"
-
+  if [ $TRANSPILE_MODE -eq 1 ]; then
+    # ./target/debug/husk compile "$file" | node > "$TEMP_STDOUT" 2> "$TEMP_STDERR"
+    SCRIPT_PATH="$(realpath "$0")"
+    SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
+    $SCRIPT_DIR/transpile.sh "$file" > "$TEMP_STDOUT" 2> "$TEMP_STDERR"
+  else
+    ./target/debug/husk "$file" > "$TEMP_STDOUT" 2> "$TEMP_STDERR"
+  fi
   echo "$base_name"
 
   # Compare stdout if the .out file exists
-  if [ -f "$SCRIPT_DIR/$base_name.out" ]; then
-    if diff --color=always "$TEMP_STDOUT" "$SCRIPT_DIR/$base_name.out" > /dev/null; then
+  if [ $TRANSPILE_MODE -eq 1 ]; then
+    out_file="$SCRIPT_DIR/$base_name.tr.out"
+  else
+    out_file="$SCRIPT_DIR/$base_name.out"
+  fi
+
+  if [ -f "$out_file" ]; then
+    if diff --color=always "$TEMP_STDOUT" "$out_file" > /dev/null; then
       echo "  stdout ✅ "
     else
       echo "  stdout ❌"
-      diff "$TEMP_STDOUT" "$SCRIPT_DIR/$base_name.out"
+      diff "$TEMP_STDOUT" "$out_file"
       ANY_TEST_FAILED=1
     fi
   else
     echo "  skipped stdout"
   fi
-
+  
   # Compare stderr if the .err file exists
-  if [ -f "$SCRIPT_DIR/$base_name.err" ]; then
-    if diff --color=always "$TEMP_STDERR" "$SCRIPT_DIR/$base_name.err" > /dev/null; then
+  if [ $TRANSPILE_MODE -eq 1 ]; then
+    err_file="$SCRIPT_DIR/$base_name.tr.err"
+  else
+    err_file="$SCRIPT_DIR/$base_name.err"
+  fi
+
+  if [ -f "$err_file" ]; then
+    if diff --color=always "$TEMP_STDERR" "$err_file" > /dev/null; then
       echo "  stderr ✅ "
     else
       echo "  stderr ❌"
-      diff "$TEMP_STDERR" "$SCRIPT_DIR/$base_name.err"
+      diff "$TEMP_STDERR" "$err_file"
       ANY_TEST_FAILED=1
     fi
   else
-    echo "  skipped stdout"
+    echo "  skipped stderr"
   fi
 
   echo ""
--- tests/scripts/update.sh
+++ tests/scripts/update.sh
@@ -14,33 +14,49 @@ while [[ "$#" -gt 0 ]]; do
     shift
 done
 
-# Loop through all .hk files in the specified directory
-for file in "$SCRIPT_DIR"/*.hk;
-do
-  # Check if there are any .hk files
-  if [ ! -e "$file" ]; then
-    echo "No .hk files found in $SCRIPT_DIR."
-    exit 1
-  fi
-
-  # Extract the base name of the file (without extension)
-  base_name=$(basename "$file" .hk)
-
-  # Paths for .out and .err files
-  out_file="$SCRIPT_DIR/$base_name.hk.out"
-  err_file="$SCRIPT_DIR/$base_name.hk.err"
-
-  # Check if the .out and .err files already exist
-  if [ -f "$out_file" ] || [ -f "$err_file" ]; then
-    if [ "$FORCE_UPDATE" = false ]; then
-      echo "$base_name.hk.out and/or $base_name.hk.err exists, skipping."
-      continue
+# Function to run command and update files
+update_files() {
+    local file=$1
+    local out_file=$2
+    local err_file=$3
+    local command=$4
+    
+    if [ -f "$out_file" ] || [ -f "$err_file" ]; then
+        if [ "$FORCE_UPDATE" = false ]; then
+            echo "$(basename "$out_file") and/or $(basename "$err_file") exists, skipping."
+            return
+        fi
     fi
-  fi
+    
+    eval "$command" > "$out_file" 2> "$err_file"
+    echo "Updated $(basename "$out_file") and $(basename "$err_file")"
+}
 
-  # Run the command and update .out and .err files
-  cargo run -q -- "$file" > "$out_file" 2> "$err_file"
-
-  echo "Updated $base_name.hk.out and $base_name.hk.err"
+# Loop through all .hk files in the specified directory
+for file in "$SCRIPT_DIR"/*.hk; do
+    # Check if there are any .hk files
+    if [ ! -e "$file" ]; then
+        echo "No .hk files found in $SCRIPT_DIR."
+        exit 1
+    fi
+    
+    # Extract the base name of the file (without extension)
+    base_name=$(basename "$file" .hk)
+    
+    # Paths for normal .out and .err files
+    out_file="$SCRIPT_DIR/$base_name.hk.out"
+    err_file="$SCRIPT_DIR/$base_name.hk.err"
+    
+    # Paths for transpiled .out and .err files
+    tr_out_file="$SCRIPT_DIR/$base_name.hk.tr.out"
+    tr_err_file="$SCRIPT_DIR/$base_name.hk.tr.err"
+    
+    # Update normal version
+    update_files "$file" "$out_file" "$err_file" "cargo run -q -- \"$file\""
+    
+    # Update transpiled version
+    # update_files "$file" "$tr_out_file" "$tr_err_file" "cargo run -q -- compile \"$file\" | node"
+    SCRIPT_PATH="$(realpath "$0")"
+    SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
+    update_files "$file" "$tr_out_file" "$tr_err_file" "$SCRIPT_DIR/transpile.sh \"$file\""
 done
-
