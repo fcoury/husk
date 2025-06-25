a34b943b07c4eccbe97d0e4babb6818b9ae73710
Felipe Coury <felipe.coury@gmail.com>
2025-06-24 21:17:48 -0300

    chore: removed cli-tool js files

--- examples/cli-tool/run_test.js
+++
@@ -1,3 +0,0 @@
-import { main } from './test_local_import.js';
-
-main();
--- examples/cli-tool/utils.js
+++
@@ -1,3 +0,0 @@
-error: 59:26 - Expected ')' after expression
-        match (self.level, level) {
-                         ^
